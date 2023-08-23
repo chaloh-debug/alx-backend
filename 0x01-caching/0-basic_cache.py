d86e772666fdf05cadac6f8e9db6aa3bc3fe71c3
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-08-23 04:52:24 +0300

    pycodestyle

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,10 +5,13 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
+    """ Implements basic objects to store and retrieve a dictionary. """
     def put(self, key, item):
+        """ Store item to a dictioary. """
         if key is None or item is None:
             pass
         self.cache_data[key] = item
 
     def get(self, key):
+        """ Retrieve item by key. """
         return self.cache_data.get(key, None)

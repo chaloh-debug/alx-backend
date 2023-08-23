571a08032b24d9542417c7b77984a30c6a1990d4
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-08-23 04:59:00 +0300

    pycodestyle

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,7 +18,7 @@ class FIFOCache(BaseCaching):
         if key is None or item is None:
             return
         self.cache_data[key] = item
-        if len(self.cache_data) >  BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             first_in, _ = self.cache_data.popitem(last=False)
             print("DISCARD", first_in)
 
